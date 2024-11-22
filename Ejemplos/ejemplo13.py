ffeea5a6c429013cb642ac721cb8aba8302926c4
crescencioesono <nveandemecrescencioesono@gmail.com>
2024-11-22 16:45:49 +0100

    New homeworks

--- Ejemplos/ejemplo13.py
+++ Ejemplos/ejemplo13.py
@@ -1,13 +1,32 @@
-while True:
+menu = """
+    1. Consultar una palabra
+    2. Agregar nueva palabra
+    3. Actualizar palabra
+    4. Eliminar palabra
+    5. Salir
+"""
+print(menu)
+opcion = input("Qué operación quieres realizar? ")
+while opcion != "5":
     try:
-        respuesta = input("Quieres agregar una palabras diccionario? ")
-        respuesta = respuesta.lower()
-        if respuesta == "si":
+        if opcion == "1":
+            palabra = input("Introduce la palabra: ")
+            with open("diccionario.txt", "r") as diccionario:
+                contenido = diccionario.read()
+                contenido = contenido.split("\n")
+                diccionario2 = {}
+                for busqueda in contenido:
+                    lista = busqueda.split(":")
+                    diccionario2[lista[0]] = lista[1]
+                print(f"{palabra}: {diccionario2[palabra]}")
+                print(menu)
+                opcion = input("Qué operación quieres realizar? ")
+        if opcion == "2":
             palabra = input("Introduce la palabra: ")
             significado = input("Introduce el significado: ")
-            with open("diccionario", "a") as diccionario:
+            with open("diccionario.txt", "a", encoding="utf-8") as diccionario:
                 diccionario.write(f"{palabra}: {significado}\n")
-        else:
-            break
+                print(menu)
+                opcion = input("Qué operación quieres realizar? ")
     except FileNotFoundError:
         open("diccionario.txt", "x")
